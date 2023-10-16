cb151a2911170ed899955c91faa61c9580a7830f
Chiu <zsh0908@outlook.com>
2023-10-16 14:41:37 +0800

    Start

---
+++ app/src/main/java/com/hfut/schedule/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.hfut.schedule
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hfut.schedule.ui.theme.肥工课程表Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
