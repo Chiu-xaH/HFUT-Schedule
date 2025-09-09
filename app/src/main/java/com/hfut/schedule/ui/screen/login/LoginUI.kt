5be39b85ba5582388b9e78bb1c038d71b365fa2e
Chiu-xaH <zsh0908@outlook.com>
2025-09-09 13:04:26 +0800

    4.17.7.1f

--- app/src/main/java/com/hfut/schedule/ui/screen/login/LoginUI.kt
+++ app/src/main/java/com/hfut/schedule/ui/screen/login/LoginUI.kt
@@ -75,7 +75,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
