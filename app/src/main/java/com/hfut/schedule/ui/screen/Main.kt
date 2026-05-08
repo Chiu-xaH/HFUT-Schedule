13d5cf49bd02dbabbce921efe6e292c9343a4f39
Chiu-xaH <zsh0908@outlook.com>
2026-05-08 14:50:09 +0800

    fix:跟随SharedNav修正一些特性

--- app/src/main/java/com/hfut/schedule/ui/screen/Main.kt
+++ app/src/main/java/com/hfut/schedule/ui/screen/Main.kt
@@ -154,7 +154,7 @@ fun MainHost(
         SharedNavHost(
             navController = navigationController,
             dependencies = dependencies,
-            modifier = Modifier.background(MaterialTheme.colorScheme.onSurface)
+            modifier = Modifier.background(MaterialTheme.colorScheme.surface)
         ) {
             val registry = LocalSharedRegistry.current
 
